4dd989351f125a77a4fd5c9124202f2c0998504e
hs96300k <hs96300k@gmail.com>
2025-04-02 20:59:12 +0500

    made-responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { FaSearch } from "react-icons/fa";
-import "./Header.css"; // Import the CSS file
-import Logo from "../../assets/logo.png"
+import { FaSearch, FaBars } from "react-icons/fa";
+import "./Header.css";
+import Logo from "../../assets/logo.png";
 
 const Header = () => {
   const [selectedMenu, setSelectedMenu] = useState("home");
+  const [menuVisible, setMenuVisible] = useState(false);
 
   const menuItems = [
     { name: "Home", id: "home" },
@@ -15,24 +16,24 @@ const Header = () => {
     { name: "Contact", id: "contact" },
   ];
 
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return (
     <header className="header">
       <div className="container">
         {/* Logo */}
-        <img className="logo" src={Logo}/>
+        <img className="logo" src={Logo} alt="Logo" />
 
         {/* Search Bar */}
         <div className="search-bar">
           <FaSearch className="search-icon" />
-          <input
-            type="text"
-            placeholder="Search"
-            className="search-input"
-          />
+          <input type="text" placeholder="Search" className="search-input" />
         </div>
 
-        {/* Menu Items */}
-        <nav>
+        {/* Desktop Menu */}
+        <nav className="desktop-menu">
           <ul className="menu">
             {menuItems.map((item) => (
               <li
@@ -47,6 +48,27 @@ const Header = () => {
             ))}
           </ul>
         </nav>
+
+        {/* Hamburger Menu Icon */}
+        <FaBars className="hamburger-menu" onClick={toggleMenu} />
+
+        {/* Menu Popup */}
+        <div className={`menu-popup ${menuVisible ? "show" : ""}`}>
+          <ul className="mobile-menu">
+            {menuItems.map((item) => (
+              <li
+                key={item.id}
+                className="mobile-menu-item"
+                onClick={() => {
+                  setSelectedMenu(item.id);
+                  setMenuVisible(false); // Close menu after clicking an item
+                }}
+              >
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </header>
   );
